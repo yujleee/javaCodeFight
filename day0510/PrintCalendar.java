0f99c799a4de8b5f4a66ae5db4860d1b3094805a
YJ LEE <82587107+yujleee@users.noreply.github.com>
2021-05-12 08:01:34 +0900

    Update PrintCalendar.java
    배열을 이용해 월의 마지막 일 구하는 것으로 변경.
    윤년일 때의 경우 추가

--- day0510/PrintCalendar.java
+++ day0510/PrintCalendar.java
@@ -1,5 +1,3 @@
-package exam07;
-
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
@@ -8,44 +6,61 @@ public class PrintCalendar {
 
    public static void getCalender(int year, int month) {
 	   
-	   if(month >= 12) {
-			year ++;
-			month %= 12;
-		} //2022까지만 정상작동 그 이상 연도추가 안됨 
-		
-		if(month <= -1) {
-			year --;			
-			month += 12;
-		}//2020까지만 정삭작동함 그 이하의 연도감소 안됨 
 	   
       GregorianCalendar startDay = new GregorianCalendar();
       startDay.set(year, month, 1);
-      int day = startDay.get(Calendar.DAY_OF_WEEK);
-
-      int date = 28;
-      int realMonth = month+1; //month가 0부터 시작하므로 +1 
-
-      if(realMonth == 1 || realMonth == 3 || realMonth == 5 || realMonth == 7 || realMonth == 8 || realMonth == 10 || realMonth == 12) {
-         date = 31;
-      } else if (realMonth == 4 || realMonth == 6 || realMonth == 9 || realMonth == 11) {
-         date = 30;
+     
+      int []lastDate = {31,28,31,30,31,30,31,31,30,31,30,31};
+      int last = lastDate[month];
+      
+      //윤년일때의 경우
+      if(month == 1 && startDay.isLeapYear(year)) {
+    	  last ++;
       }
 
+      int day = startDay.get(Calendar.DAY_OF_WEEK);
+    
 
-      System.out.println(year + "년 " + (realMonth) + "월");
+      System.out.println(year + "년 " + (month+1) + "월");
       //달력모양
       System.out.println("일\t월\t화\t수\t목\t금\t토");
 
       for (int i = 1; i < day; i++) {
          System.out.print("\t");
       }
-      for (int i = 1; i <= date; i++) {
+      for (int i = 1; i <= last; i++) {
          System.out.print(i + "\t");
 
          if((i+day-1) % 7 == 0) {
             System.out.println();
          }
       }
+      
+      Scanner sc = new Scanner(System.in);
+      System.out.print("\n 1. 이전달 달력 출력   2. 다음달 달력 출력     3. 종료 ====>");
+      int num = sc.nextInt();
+
+  
+      switch(num) {
+      case 1: month--; break;
+      case 2: month++; break;
+      case 3: return;
+      }
+      
+  	// 작년, 내년으로 갈 경우
+		if(month == -1) {
+			year --;
+			month = 11;
+		}
+		
+		if(month == 12) {
+			year ++;
+			month = 0;
+		}
+		
+		//재귀호출
+		getCalender(year, month);
+      
    }
 
 
@@ -55,22 +70,6 @@ public class PrintCalendar {
       int month = today.get(Calendar.MONTH);
 
       getCalender(year,month);
-
-      while(true) {
-         Scanner sc = new Scanner(System.in);
-         System.out.print("\n 1. 이전달 달력 출력   2. 다음달 달력 출력     3. 종료 ====>");
-         int num = sc.nextInt();
-
-         if(num == 3) {
-            break;
-         }
-
-         switch(num) {
-         case 1: getCalender(year, (month-= 1) ); break;
-         case 2:   getCalender(year, (month+= 1) ); break;
-         }
-
-      }
 
    }
 
