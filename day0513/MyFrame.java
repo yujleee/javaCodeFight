8ab24c4d393ebcd15fb1fb0a76efb2325758a608
YJ LEE <82587107+yujleee@users.noreply.github.com>
2021-05-13 23:24:23 +0900

    0513 TIL

--- day0513/MyFrame.java
+++ day0513/MyFrame.java
@@ -1,8 +1,19 @@
-public class MyFrameTest {
+import javax.swing.JFrame;
 
-	public static void main(String[] args) {
-		MyFrame mf = new MyFrame();
+public class MyFrame extends JFrame {
 
+	RedBallPanel rbp;
+	
+	public MyFrame() {
+		rbp = new RedBallPanel();
+		(new Thread(rbp)).start(); //start의 위치는 RedBallPanel의 생성자여도 상관 없음 
+		add(rbp);
+		//판넬 => 기본이 flow. Frame은 borderLayout
+		
+		setSize(400, 300);
+		setVisible(true);
+		
+		
 	}
-
+	
 }
